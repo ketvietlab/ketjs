8f94d64fd8ebdf237be16c0533a4fc7d4e26437d
duykieu <hello@ketviet.vn>
2026-08-21 00:13:56 +0700

    feat(ketsuite): refine customer invoice form

--- apps/ketsuite/app.ts
+++ apps/ketsuite/app.ts
@@ -70,6 +70,8 @@ export const ketsuite = defineApp({
     suite.stockLotActivityBackend,
     suite.stockMailInbound,
     suite.accountBackend,
+    suite.accountMailBackend,
+    suite.accountActivityBackend,
     suite.purchaseBackend,
     suite.saleBackend,
     suite.saleMailBackend,
